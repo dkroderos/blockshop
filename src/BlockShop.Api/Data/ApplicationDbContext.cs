b39bb33af10b2dd106cc3ba4ce46fd09b711e257
David King Roderos <dk.roderos@gmail.com>
2024-05-09 12:11:41 +0800

    Implement identity

---
+++ src/BlockShop.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlockShop.Api.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options);
--- src/BlockShop.Api/Program.cs
+++ src/BlockShop.Api/Program.cs
@@ -1,8 +1,20 @@
+using BlockShop.Api.Data;
+using BlockShop.Api.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -10,14 +22,20 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
+    app.ApplyMigrations();
 }
+
+app.MapIdentityApi<IdentityUser>();
 
 app.UseCors(x => x
     .AllowAnyMethod()
     .AllowAnyHeader()
-    .SetIsOriginAllowed(origin => true)
+    .SetIsOriginAllowed(_ => true)
     .AllowCredentials());
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 app.Run();
