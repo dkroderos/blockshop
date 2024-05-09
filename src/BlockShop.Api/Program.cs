2577af90bf32329e4035670021b216a67aabee23
David King Roderos <dk.roderos@gmail.com>
2024-05-09 12:19:15 +0800

    Implement swagger auth

--- src/BlockShop.Api/Program.cs
+++ src/BlockShop.Api/Program.cs
@@ -2,11 +2,23 @@ using BlockShop.Api.Data;
 using BlockShop.Api.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 builder.Services.AddCors();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
